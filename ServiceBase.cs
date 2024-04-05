ef1b70dbc14df020c88bd5d2ab863f8032fadb60
yunix-dev <57519232+yunix-dev@users.noreply.github.com>
2024-04-05 17:20:27 +0900

    Add files via upload

---
+++ ServiceBase.cs
@@ -0,0 +1,77 @@
+﻿using EagleEye.Data;
+using EagleEye.Data.Entities.Devices;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EagleEye.UI.Web.Core.Services
+{
+    public abstract class ServiceBase<T, Key> : IServiceBase<T> where T : Data.Entities.Entity<Key>
+    {
+        public IdentityContext Context { get; set; }
+        public DbSet<T> Entity { get; set; }
+
+        public ServiceBase(IdentityContext context)
+        {
+            this.Context = context;
+            if (this.Context != null)
+            {
+                this.Entity = this.Context.Set<T>();
+            }
+        }
+
+        public virtual T Delete(T item)
+        {
+            this.Entity.Remove(item);
+            this.Context.SaveChanges();
+            return item;
+        }
+
+        public virtual T Get(T item)
+        {
+            return this.Entity.Find(item.Id);
+        }
+
+        public virtual IQueryable<T> GetPaging(int start, int length)
+        {
+            return this.Entity.Skip(start).Take(length);
+        }
+
+        public virtual List<T> GetAll()
+        {
+            return this.Entity.ToList();
+        }
+
+        public virtual T Insert(T item)
+        {
+            this.Entity.Add(item);
+            this.Context.SaveChanges();
+            return item;
+        }
+
+        //public virtual async Task<T> InsertAsync(T item)
+        //{
+        //    this.Entity.Add(item);
+        //    await this.Context.SaveChangesAsync();
+        //    return item;
+        //}
+
+        public virtual T Update(T item)
+        {
+            this.Entity.Update(item);
+            this.Context.SaveChanges();
+            return item;
+        }
+
+        protected DbConnection GetConnection()
+        {
+            var connection = this.Context.Database.GetDbConnection() as DbConnection;
+            return connection;
+        }
+    }
+}
---
+++ ServiceDapperBase.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EagleEye.UI.Web.Core.Services
+{
+    public abstract class ServiceDapperBase<T, Key> : IServiceBase<T> where T : Data.Entities.Entity<Key>
+    {
+        protected readonly IConfiguration _configuration;
+
+        public abstract T Delete(T item);
+        public abstract T Get(T item);
+        public abstract List<T> GetAll();
+        public abstract T Insert(T item);
+
+        public abstract IQueryable<T> GetPaging(int start, int length);
+        //public virtual async Task<T> InsertAsync(T item)
+        //{
+        //    await Task.Delay(0);
+        //    var task = Task.Factory.StartNew(() => {
+        //        return this.Insert(item);
+        //    });
+
+        //    return task.Result;
+        //}
+        public abstract T Update(T item);
+
+        public ServiceDapperBase(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            var conn = new SqlConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
